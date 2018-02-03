b610d456e1b3dc6ac326172904a9318598fb9400
Bjoern Doebel <bjoern.doebel@gmail.com>
2018-02-03 22:56:17 +0000

    Add path parameter
    Allow specification of 0 or more paths as parameters. Add a function
    that translates these parameters into a vector of strings (and adds a
    "." when nothing else is specified).
    
    Signed-off-by: Bjoern Doebel <bjoern.doebel@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,23 @@ extern crate loggerv;
 
 use clap::App;
 
+fn get_path_vector<'a>(app : &'a clap::ArgMatches) -> Vec<&'a str>
+{
+    match app.values_of("paths") {
+        None => vec!["."],
+        Some(t) => t.collect(),
+    }
+}
+
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let params = App::from_yaml(yaml).get_matches();
     loggerv::init_with_verbosity(params.occurrences_of("verbose")).unwrap();
     warn!("verbosity = {}", params.occurrences_of("verbose"));
-    info!("Hello world (info)");
-    warn!("Hello world (warn)");
-    error!("Hello world (error)");
+
+    info!("Path parameters:");
+    let paths = get_path_vector(&params);
+    for path in paths {
+        info!("  - {}", path);
+    }
 }

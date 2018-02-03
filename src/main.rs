ead0bf34785d6eb809f9a109c5778d1370c94d2e
Bjoern Doebel <bjoern.doebel@gmail.com>
2018-02-03 20:15:20 +0000

    Log verbosity on startup
    Signed-off-by: Bjoern Doebel <bjoern.doebel@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ use clap::App;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
-    loggerv::init_with_verbosity(matches.occurrences_of("verbose")).unwrap();
+    let params = App::from_yaml(yaml).get_matches();
+    loggerv::init_with_verbosity(params.occurrences_of("verbose")).unwrap();
+    warn!("verbosity = {}", params.occurrences_of("verbose"));
     info!("Hello world (info)");
     warn!("Hello world (warn)");
     error!("Hello world (error)");

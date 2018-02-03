f4d205f849cf1e9ee8daf1bc7b967caaffcc03b1
Bjoern Doebel <bjoern.doebel@gmail.com>
2018-02-03 19:56:13 +0000

    loggerv for logging
    Signed-off-by: Bjoern Doebel <bjoern.doebel@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#[macro_use] extern crate log;
+extern crate loggerv;
+
 fn main() {
-    println!("Hello, world!");
+    loggerv::init_with_verbosity(1).unwrap();
+    info!("Hello world (info)");
+    warn!("Hello world (warn)");
+    error!("Hello world (error)");
 }

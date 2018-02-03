e195d4ba9d11d0bdfe553cdd2593d1eb6372ef58
Bjoern Doebel <bjoern.doebel@gmail.com>
2018-02-03 20:09:49 +0000

    Clap for command line parsing
    Signed-off-by: Bjoern Doebel <bjoern.doebel@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 #[macro_use] extern crate log;
+#[macro_use] extern crate clap;
 extern crate loggerv;
 
+use clap::App;
+
 fn main() {
-    loggerv::init_with_verbosity(1).unwrap();
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    loggerv::init_with_verbosity(matches.occurrences_of("verbose")).unwrap();
     info!("Hello world (info)");
     warn!("Hello world (warn)");
     error!("Hello world (error)");
